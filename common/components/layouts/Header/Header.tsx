5d44c5978ddf39cbe3906eb71af2ce5c9aff37e9
yeji yu <yeji.yu@concentrix.com>
2025-09-05 19:25:52 +0900

    Footer 반응형 + 헤더 pc까지

--- common/components/layouts/Header/Header.tsx
+++ common/components/layouts/Header/Header.tsx
@@ -88,7 +88,7 @@ export default function Header() {
 
     window.addEventListener('scroll', handleScroll);
 
-    setMounted(true); // Set mounted to true after initial render
+    setMounted(true);
 
     // 컴포넌트 언마운트 시 이벤트 리스너 제거
     return () => {
