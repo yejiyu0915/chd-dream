83aeeda170ea33d95ee02d83adeec9de7b05ae07
yeji yu <yeji.yu@concentrix.com>
2025-11-05 10:42:56 +0900

    빌드에러 수정

--- app/info/c-log/page.tsx
+++ app/info/c-log/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { getCLogData } from '@/lib/notion';
 import CLogListClient from '@/app/info/c-log/components/CLogListClient';
 
@@ -6,6 +7,10 @@ export default async function CLogListPage() {
   // 서버에서 C-Log 데이터를 병렬로 가져옴 (빠른 초기 로딩)
   const cLogData = await getCLogData();
 
-  // 클라이언트 컴포넌트에 서버 데이터를 전달하여 즉시 렌더링
-  return <CLogListClient initialCLogData={cLogData} />;
+  // useSearchParams()를 사용하므로 Suspense로 감싸기 (Next.js 15 요구사항)
+  return (
+    <Suspense fallback={<div>로딩 중...</div>}>
+      <CLogListClient initialCLogData={cLogData} />
+    </Suspense>
+  );
 }
